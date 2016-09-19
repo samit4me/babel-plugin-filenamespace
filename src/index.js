fd2003e5c15e0a272d9084f8829925b0844628c2
Samuel Sharpe <Samuel Sharpe>
2016-09-19 19:51:08 +1000

    Stop adding the trailing separator

--- src/index.js
+++ src/index.js
@@ -1,29 +1,35 @@
 import nodePath from 'path';
 
-export default function ({ types: t }) {
-  return {
-    visitor: {
-      Identifier(path, state) {
-        if (path.node.name === '__filenamespace') {
-          const { seperator, root } = state.opts;
-          const { filename, basename, sourceRoot } = state.file.opts;
-          const dirname = nodePath.dirname(filename);
-          let filenamespace = dirname.replace(`${sourceRoot}/`, '');
-          if (root && filenamespace.startsWith(root)) {
-            filenamespace = filenamespace.replace(`${root}/`, '');
-          }
-          if (basename !== 'index') {
-            filenamespace += `/${basename}`;
-          }
-          if (seperator) {
-            filenamespace = filenamespace.replace(/\//g, seperator);
-            filenamespace += seperator;
-          } else {
-            filenamespace += '/';
-          }
-          path.replaceWith(t.stringLiteral(filenamespace));
+export default ({ types: t }) => ({
+  visitor: {
+    Identifier(path, state) {
+      if (path.node.name === '__filenamespace') {
+        // Get user settings
+        const { seperator, root } = state.opts;
+
+        // Get file paths
+        const { filename, basename, sourceRoot } = state.file.opts;
+        const dirname = nodePath.dirname(filename);
+
+        // Transform filename
+        let filenamespace = dirname.replace(`${sourceRoot}/`, '');
+        if (root && filenamespace.startsWith(root)) {
+          filenamespace = filenamespace.replace(`${root}/`, '');
         }
-      },
+
+        // Remove filename if "index" as it is meaningless
+        if (basename !== 'index') {
+          filenamespace += `/${basename}`;
+        }
+
+        // Set custom seperator
+        if (seperator) {
+          filenamespace = filenamespace.replace(/\//g, seperator);
+        }
+
+        // Replace __filenamespace with the transformed value
+        path.replaceWith(t.stringLiteral(filenamespace));
+      }
     },
-  };
-}
+  },
+});
