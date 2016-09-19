7ac52dc7deb22d179ffc6e9705bad3fdaa483f22
Samuel Sharpe <Samuel Sharpe>
2016-09-19 18:38:00 +1000

    Add ESLint

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import nodePath from 'path';
 
-export default function({ types: t }) {
+export default function ({ types: t }) {
   return {
     visitor: {
       Identifier(path, state) {
-        if (path.node.name === "__filenamespace") {
+        if (path.node.name === '__filenamespace') {
           const { seperator, root } = state.opts;
           const { filename, basename, sourceRoot } = state.file.opts;
           const dirname = nodePath.dirname(filename);
@@ -23,7 +23,7 @@ export default function({ types: t }) {
           }
           path.replaceWith(t.stringLiteral(filenamespace));
         }
-      }
-    }
+      },
+    },
   };
 }
